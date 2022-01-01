27f9b387f83f6febf25043a0bcaada71bcb40513
mingyuk99 <mingyu5675@gmail.com>
2022-01-01 23:20:09 +0900

    file practice - 2

--- FileStream/app/src/main/java/com/example/filestream/MainActivity.kt
+++ FileStream/app/src/main/java/com/example/filestream/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.filestream
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,9 +15,10 @@ class MainActivity : AppCompatActivity() {
         val textField = findViewById<EditText>(R.id.textField)
         val btnSave = findViewById<Button>(R.id.btnSave)
         val btnLoad = findViewById<Button>(R.id.btnLoad)
+//        val filepath = android.os.Environment.getExternalStorageDirectory().getAbsolutePath()
 
-        val filename = "data.txt"
-
+        val filename = "file.txt"
+        val filepath = filesDir.toString()
         btnSave.setOnClickListener {
             val text = textField.text.toString()
 
@@ -27,11 +30,14 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun saveFile(text: String, filename: String){
+    private fun saveFile(text: String, filename: String){
         val fos = openFileOutput(filename, MODE_PRIVATE)
         fos.write(text.toByteArray())
 
         fos.close()
+
+//        val filelist = fileList()
+//        Log.d("File", ""+filelist)
     }
 
     fun loadFile(filename: String): String{
