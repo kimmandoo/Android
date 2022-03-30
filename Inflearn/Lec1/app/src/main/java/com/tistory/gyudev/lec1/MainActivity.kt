70aa248ce3ca565f4293d130d1389db0b819318f
mingyuk99 <mingyu5675@gmail.com>
2022-03-30 15:57:18 +0900

    Lec1 fin

--- Inflearn/Lec1/app/src/main/java/com/tistory/gyudev/lec1/MainActivity.kt
+++ Inflearn/Lec1/app/src/main/java/com/tistory/gyudev/lec1/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.tistory.gyudev.lec1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val img1 = findViewById<ImageView>(R.id.bts_1)
+
+        val intent = Intent(this, ImageActivity::class.java)
+
+        img1.setOnClickListener {
+            Toast.makeText(this, "bts1_Clicked", Toast.LENGTH_SHORT).show()
+
+            startActivity(intent)
+        }
+
     }
 }
--- Inflearn/README.md
+++ Inflearn/README.md
@@ -2,3 +2,6 @@
 
 - Linear Layout
 - ImageView
+- Toast
+- setOnClickListener
+- Intent(단순 액티비티 전환)
