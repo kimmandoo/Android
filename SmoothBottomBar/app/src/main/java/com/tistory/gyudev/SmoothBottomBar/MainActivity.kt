5a3c02b64c3d2794ab915ba645ee70126ff324b1
mingyuk99 <mingyu5675@gmail.com>
2022-05-14 23:10:20 +0900

    smoothbottombar practice

---
+++ SmoothBottomBar/app/src/main/java/com/tistory/gyudev/SmoothBottomBar/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.tistory.gyudev.smoothbottombar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.PopupMenu
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import me.ibrahimsn.lib.SmoothBottomBar
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
