e85c24025a599492516b4803505d49da1f135d9c
kimmandoo <mingyu5675@gmail.com>
2022-06-24 23:03:36 +0900

    bottom_nav_menu fragment setting done

--- Udemy/Dorisgram/app/src/main/java/com/kimmandoo/MainActivity.kt
+++ Udemy/Dorisgram/app/src/main/java/com/kimmandoo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.kimmandoo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -12,6 +13,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.mainBottomNav.itemIconTintList = null // 하트 틴트 자동으로 칠해지던거 없애줌
         setContentView(binding.root)
+        supportFragmentManager.beginTransaction().replace(R.id.main_frame_layout, HomeFragment()).commitAllowingStateLoss()
+
+        binding.mainBottomNav.setOnItemSelectedListener { item->
+            when(item.itemId){
+                R.id.tab_home ->{
+                    supportFragmentManager.beginTransaction().replace(R.id.main_frame_layout, HomeFragment()).commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.tab_search ->{
+                    supportFragmentManager.beginTransaction().replace(R.id.main_frame_layout, SearchFragment()).commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.tab_upload ->{
+                    val intent = Intent(this, UploadActivity::class.java)
+                    startActivity(intent)
+                    return@setOnItemSelectedListener true
+                }
+                R.id.tab_heart ->{
+                    supportFragmentManager.beginTransaction().replace(R.id.main_frame_layout, HeartFragment()).commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.tab_mypage ->{
+                    supportFragmentManager.beginTransaction().replace(R.id.main_frame_layout, MyPageFragment()).commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
     }
 }
