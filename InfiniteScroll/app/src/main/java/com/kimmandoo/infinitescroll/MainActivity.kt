ed48a11468574cdb4c9f89d3d15e5e336b588eea
mingyuk99 <mingyu5675@gmail.com>
2022-07-12 18:03:38 +0900

    되다 만 무한 스크롤뷰

--- InfiniteScroll/app/src/main/java/com/kimmandoo/infinitescroll/MainActivity.kt
+++ InfiniteScroll/app/src/main/java/com/kimmandoo/infinitescroll/MainActivity.kt
@@ -2,10 +2,68 @@ package com.kimmandoo.infinitescroll
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.kimmandoo.infinitescroll.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private var page = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        val items = arrayListOf<String>(
+            "this",
+            "this is ",
+            "this is good",
+            "this",
+            "this is ",
+            "this is good",
+            "this",
+            " ",
+            "this is good",
+            "this",
+            "this is ",
+            "this is good",
+            "this",
+            "this is ",
+            "this is good",
+            "this",
+            "this is ",
+            "this is good",
+            "this",
+            "this is ",
+            "this is good",
+            "this",
+            "this is ",
+            "this is good",
+            "this",
+            "this is ",
+            "this is good",
+        )
+        val rvAdapter = RecyclerViewAdapter()
+        rvAdapter.setList(items)
+
+        binding.rv.apply {
+            rvAdapter.notifyItemRangeInserted((page - 1) * 10, 10)
+            binding.rv.layoutManager = LinearLayoutManager(context)
+            binding.rv.adapter = rvAdapter
+        }
+
+        binding.rv.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val lastVisibleItemPosition =
+                    (recyclerView.layoutManager as LinearLayoutManager?)!!.findLastCompletelyVisibleItemPosition()
+                val itemTotalCount = recyclerView.adapter!!.itemCount - 1
+
+                if (!binding.rv.canScrollVertically(1) && lastVisibleItemPosition == itemTotalCount) {
+                    RecyclerViewAdapter().deleteLoading()
+                    rvAdapter.notifyDataSetChanged()
+                }
+            }
+        })
+
     }
 }
---
+++ InfiniteScroll/app/src/main/java/com/kimmandoo/infinitescroll/RecyclerViewAdapter.kt
@@ -0,0 +1,72 @@
+package com.kimmandoo.infinitescroll
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.kimmandoo.infinitescroll.databinding.RvItemBinding
+import com.kimmandoo.infinitescroll.databinding.RvLoadingBinding
+
+class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+
+    private val VIEW_TYPE_ITEM = 0
+    private val VIEW_TYPE_LOADING = 1
+    private val items = ArrayList<String>()
+
+    // 아이템뷰에 게시물이 들어가는 경우
+    inner class NoticeViewHolder(private val binding: RvItemBinding):RecyclerView.ViewHolder(binding.root){
+        fun bind(text: String){
+            binding.rvItemTv.text = text
+        }
+    }
+
+    // 아이템뷰에 프로그레스바가 들어가는 경우
+    inner class LoadingViewHolder(private val binding: RvLoadingBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    // view type setter
+    override fun getItemViewType(position: Int): Int {
+        // 게시물과 프로그레스바 아이템뷰를 구분할 기준이 필요하다.
+        return when (items[position]) {
+            " " -> VIEW_TYPE_LOADING
+            else -> VIEW_TYPE_ITEM
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            VIEW_TYPE_ITEM -> {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = RvItemBinding.inflate(layoutInflater, parent, false)
+                NoticeViewHolder(binding)
+            }
+            else -> {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = RvLoadingBinding.inflate(layoutInflater, parent, false)
+                LoadingViewHolder(binding)
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if(holder is NoticeViewHolder){
+            holder.bind(items[position])
+        }else{
+
+        }
+    }
+
+    fun setList(text: MutableList<String>) {
+        items.addAll(text)
+        items.add(" ") // progress bar 넣을 자리
+    }
+
+    fun deleteLoading(){
+        items.removeAt(items.lastIndex) // 로딩이 완료되면 프로그레스바를 지움
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+}
