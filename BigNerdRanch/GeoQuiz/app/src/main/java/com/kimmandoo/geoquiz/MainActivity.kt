6098a8745b131009ef9867d533ff6448b0106afe
kimmandoo <mingyu5675@gmail.com>
2022-07-26 19:18:45 +0900

    ch1 done & challange

--- BigNerdRanch/GeoQuiz/app/src/main/java/com/kimmandoo/geoquiz/MainActivity.kt
+++ BigNerdRanch/GeoQuiz/app/src/main/java/com/kimmandoo/geoquiz/MainActivity.kt
@@ -2,10 +2,26 @@ package com.kimmandoo.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Toast
+import com.kimmandoo.geoquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.btnTrue.setOnClickListener {
+            //api30 over decreaped
+            val toast = Toast.makeText(this, R.string.toast_o, Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.CENTER_VERTICAL,100,0)
+            toast.show()
+        }
+
+        binding.btnFalse.setOnClickListener {
+            Toast.makeText(this, R.string.toast_x, Toast.LENGTH_SHORT).show()
+        }
     }
 }
