ffc2bcbffc4181101763742305ca39524e1a895a
김민규 <mingyu5675@gmail.com>
2022-07-29 12:04:18 +0900

    ~56p

--- BigNerdRanch/GeoQuiz/app/src/main/java/com/kimmandoo/geoquiz/MainActivity.kt
+++ BigNerdRanch/GeoQuiz/app/src/main/java/com/kimmandoo/geoquiz/MainActivity.kt
@@ -24,13 +24,38 @@ class MainActivity : AppCompatActivity() {
 
         binding.btnTrue.setOnClickListener {
             //api30 over decreaped
-            val toast = Toast.makeText(this, R.string.toast_o, Toast.LENGTH_SHORT)
-            toast.setGravity(Gravity.CENTER_VERTICAL, 100, 0)
-            toast.show()
+//            val toast = Toast.makeText(this, R.string.toast_o, Toast.LENGTH_SHORT)
+//            toast.setGravity(Gravity.CENTER_VERTICAL, 100, 0)
+//            toast.show()
+            checkAnswer(true)
         }
 
         binding.btnFalse.setOnClickListener {
-            Toast.makeText(this, R.string.toast_x, Toast.LENGTH_SHORT).show()
+//            Toast.makeText(this, R.string.toast_x, Toast.LENGTH_SHORT).show()
+            checkAnswer(false)
         }
+
+        binding.btnNext.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+        updateQuestion()
+    }
+    //공통된 건 함수로 뺀다.
+    private fun updateQuestion(){
+        val questionTextResId = questionBank[currentIndex].textResId
+        // setText는 resId를 인자로 받을 수 있다.
+        binding.tvQuestion.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean){
+        val correctAnswer = questionBank[currentIndex].answer
+        val messageResId = if(userAnswer == correctAnswer){
+            R.string.toast_o
+        }else{
+            R.string.toast_x
+        }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
     }
 }
