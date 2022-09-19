4dc41943f8ad3d4265ddc0b1d314a41a90ba0745
mingyu5675 <mingyu5675@gmail.com>
2022-09-19 17:05:02 +0900

    foody chap1

--- Udemy/Foody/app/src/main/java/com/kimmandoo/foody/MainActivity.kt
+++ Udemy/Foody/app/src/main/java/com/kimmandoo/foody/MainActivity.kt
@@ -2,10 +2,40 @@ package com.kimmandoo.foody
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.kimmandoo.foody.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+
+//        navController = findNavController(R.id.navHostFragment)
+        navController = navHostFragment.navController
+
+        val appBarConfig: AppBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recipesFragment,
+                R.id.favoriteRecipesFragment,
+                R.id.foodJokeFragment
+            )
+        )
+
+        binding.bottomNavBar.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfig)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
---
+++ Udemy/Foody/readme.md
@@ -0,0 +1,5 @@
+#### Udemy Foody
+
+- [ ] Navigation + BottomNavigationBar
+
+- [ ] 
