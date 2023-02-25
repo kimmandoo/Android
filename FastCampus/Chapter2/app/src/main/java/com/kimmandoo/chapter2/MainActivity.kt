25adf5ae808da3c2d93b7dd56aebb3a8d33bb126
kimmandoo <mingyu5675@gmail.com>
2023-02-25 16:09:48 +0900

    ch2 끝

--- FastCampus/Chapter2/app/src/main/java/com/kimmandoo/chapter2/MainActivity.kt
+++ FastCampus/Chapter2/app/src/main/java/com/kimmandoo/chapter2/MainActivity.kt
@@ -2,10 +2,27 @@ package com.kimmandoo.chapter2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tv = findViewById<TextView>(R.id.tv)
+        val resetBtn = findViewById<Button>(R.id.btn_reset)
+        val plusBtn = findViewById<Button>(R.id.btn_plus)
+
+        var number = 0
+        resetBtn.setOnClickListener {
+            number = 0
+            tv.text = number.toString()
+        }
+
+        plusBtn.setOnClickListener {
+            number++
+            tv.text = number.toString()
+        }
     }
 }
